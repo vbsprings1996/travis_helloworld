49e8bdb374c0dcf66781978a645878278cb4ce27
Vijay Bheemineni <bheemineni.vijay@gmail.com>
2017-06-21 11:21:46 -0500

    Modified hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("Python helloworld travis ci")
-print('---'*20)
+def is_prime(number):
+
+    if number == 0 or number == 1:
+        return False
+
+    for i in range(2, number):
+        if number % i == 0:
+            #print("Number {} is not prime number.".format(number))
+            return False
+    else:
+        #print("Number {} is prime number.".format(number))
+        return True
+
+if __name__ == '__main__':
+    print('-'*20)
+    print([i for i in range(1001) if is_prime(i)])
